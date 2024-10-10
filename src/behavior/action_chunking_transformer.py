993aa66c51fcb31adae67243e685375b757cae99
John Marangola <jmarangola@softroboticsinc.com>
2024-10-10 10:35:17 -0400

    Organize functions better.

--- src/behavior/action_chunking_transformer.py
+++ src/behavior/action_chunking_transformer.py
@@ -6,53 +6,11 @@ from src.behavior.base import Actor
 from typing import Dict, Tuple, Union
 from omegaconf import DictConfig, OmegaConf
 from torch.autograd import Variable
-from typing import Optional, Any, Callable
+from typing import Optional, Any, Callable, Type
 from torch import Tensor
 from copy import deepcopy
 
 
-def reparametrize(mu, logvar):
-    """Sample from p(z) using the reparameterization trick.
-
-    Args:
-        mu (Tensor): Mean of the distribution.
-        logvar (Tensor): Log-variance of the distribution.
-
-    Returns:
-        Tensor: Sampled latent vector.
-    """
-    std = torch.exp(0.5 * logvar)
-    eps = torch.randn_like(std)
-    return mu + std * eps
-
-
-def kl_divergence(mu: torch.Tensor, logvar: torch.Tensor):
-    """Computes the kl-divergence between the approximate posterior q(z|x)
-        and the standard normal prior N(0, 1).
-
-    Args:
-        mu (torch.Tensor): Mean of approximate posterior distribution, shape (B, latent_dim)
-        logvar (torch.Tensor): Log-variance of approximate posterior distribution, shape (B, latent_dim)
-
-    Returns:
-        Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-          - total_kld: The total kl divergence, summed over latent dims and averaged over the batch (minimizing this guy)
-          - dimension_wise_kld: KL divergence per latent dimension, averaged across the batch.
-          - mean_kld: Mean KL divergence, averaged over both batch and latent dimension.
-
-    Note: This function  WILL need modifications in the form of flattening/reshaping to extend to latent representations of images
-    """
-    batch_size = mu.size(0)
-    assert batch_size != 0, "Batch size must be non-zero to compute KL-divergence"
-
-    klds = -0.5 * (1 + logvar - mu.pow(2) - logvar.exp())
-    total_kld = klds.sum(1).mean(0, True)
-    dimension_wise_kld = klds.mean(0)
-    mean_kld = klds.mean(1).mean(0, True)
-
-    return total_kld, dimension_wise_kld, mean_kld
-
-# TODO: Add positional encodings for (T, J, V) temporal settings
 
 
 def _get_module_clones(module: nn.Module, n: int) -> nn.Module:
@@ -75,7 +33,7 @@ class ModuleConfigurator():
     """
     A utility class to initialize attributes from a config.
     """
-    def __init__(self, cltype: type, cfg: DictConfig, args: List[str]):
+    def __init__(self, cltype: Type, cfg: DictConfig, args: List[str]):
         clname = cltype.__name__
         if (module_cfg := self.find_recursive(cfg, clname)) is None:
             raise ValueError(f"ModuleConfigurator failed to find {clname} in input cfg '{cfg}'.")
@@ -241,6 +199,48 @@ class ActionChunkingTransformerPolicy(Actor):
         pass
 
         # return loss, loss_dict
+
+
+def reparametrize(mu, logvar):
+    """Sample from p(z) using the reparameterization trick.
+
+    Args:
+        mu (Tensor): Mean of the distribution.
+        logvar (Tensor): Log-variance of the distribution.
+
+    Returns:
+        Tensor: Sampled latent vector.
+    """
+    std = torch.exp(0.5 * logvar)
+    eps = torch.randn_like(std)
+    return mu + std * eps
+
+
+def kl_divergence(mu: torch.Tensor, logvar: torch.Tensor):
+    """Computes the kl-divergence between the approximate posterior q(z|x)
+        and the standard normal prior N(0, 1).
+
+    Args:
+        mu (torch.Tensor): Mean of approximate posterior distribution, shape (B, latent_dim)
+        logvar (torch.Tensor): Log-variance of approximate posterior distribution, shape (B, latent_dim)
+
+    Returns:
+        Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+          - total_kld: The total kl divergence, summed over latent dims and averaged over the batch (minimizing this guy)
+          - dimension_wise_kld: KL divergence per latent dimension, averaged across the batch.
+          - mean_kld: Mean KL divergence, averaged over both batch and latent dimension.
+
+    Note: This function  WILL need modifications in the form of flattening/reshaping to extend to latent representations of images
+    """
+    batch_size = mu.size(0)
+    assert batch_size != 0, "Batch size must be non-zero to compute KL-divergence"
+
+    klds = -0.5 * (1 + logvar - mu.pow(2) - logvar.exp())
+    total_kld = klds.sum(1).mean(0, True)
+    dimension_wise_kld = klds.mean(0)
+    mean_kld = klds.mean(1).mean(0, True)
+
+    return total_kld, dimension_wise_kld, mean_kld
 
 
 mu_var = torch.rand(3, 2)
