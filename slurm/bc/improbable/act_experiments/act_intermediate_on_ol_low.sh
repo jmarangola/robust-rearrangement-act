f147222f9cbdcc73f8e7c034472707de62fd2fad
John Marangola <jmarangola@softroboticsinc.com>
2024-10-25 15:05:50 -0400

    check in last config.

---
+++ slurm/bc/improbable/act_experiments/act_intermediate_on_ol_low.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH -p vision-pulkitag-a100,vision-pulkitag-a6000,vision-pulkitag-3090
+#SBATCH -q vision-pulkitag-free-cycles
+#SBATCH --job-name=ol_state_trans_diff
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=64GB
+#SBATCH --time=01-00:00
+#SBATCH --gres=gpu:1
+
+python -m src.train.bc +experiment=state/act \
+    randomness='[low,low_perturb]' \
+    rollout.randomness=low \
+    task=one_leg \
+    wandb.project=act-retint-low \
+    rollout.max_steps=700
