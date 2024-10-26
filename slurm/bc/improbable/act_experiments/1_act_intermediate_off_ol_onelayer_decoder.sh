62604f78c2b8397714276443fc1b3a5a973831f5
ankile <lars.ankile@gmail.com>
2024-10-26 09:55:07 -0400

    Small updates to the return intermediate debug slurm scripts

--- slurm/bc/improbable/act_experiments/1_act_intermediate_off_ol_onelayer_decoder.sh
+++ slurm/bc/improbable/act_experiments/1_act_intermediate_off_ol_onelayer_decoder.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 #SBATCH -p vision-pulkitag-a100,vision-pulkitag-a6000,vision-pulkitag-3090
-#SBATCH -q vision-pulkitag-free-cycles
-#SBATCH --job-name=ol_state_trans_diff
+#SBATCH -q vision-pulkitag-main
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=64GB
 #SBATCH --time=01-00:00
 #SBATCH --gres=gpu:1
+#SBATCH --job-name=1_ol_state_act
 
 python -m src.train.bc +experiment=state/act \
-    randomness='[low,low_perturb]' \
-    rollout.randomness=low \
+    randomness=low \
     task=one_leg \
-    dryrun=false actor.ConditionalVAE.TransformerDecoder.return_intermediate=false \
-    actor.ConditionalVAE.TransformerDecoder.num_layers=1 \
+    actor.ConditionalVAE.ActionTransformer.TransformerDecoder.return_intermediate=false \
+    actor.ConditionalVAE.ActionTransformer.TransformerDecoder.num_layers=1 \
     wandb.project=act-retint-low \
-    rollout.max_steps=700
+    rollout.max_steps=700 \
+    dryrun=false
--- slurm/bc/improbable/act_experiments/3_act_intermediate_on_ol_low.sh
+++ slurm/bc/improbable/act_experiments/3_act_intermediate_on_ol_low.sh
@@ -2,17 +2,17 @@
 
 #SBATCH -p vision-pulkitag-a100,vision-pulkitag-a6000,vision-pulkitag-3090
 #SBATCH -q vision-pulkitag-free-cycles
-#SBATCH --job-name=ol_state_trans_diff
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=64GB
 #SBATCH --time=01-00:00
 #SBATCH --gres=gpu:1
+#SBATCH --job-name=3_ol_state_act
 
 python -m src.train.bc +experiment=state/act \
-    randomness='[low,low_perturb]' \
-    rollout.randomness=low \
+    randomness=low \
     task=one_leg \
+    rollout.max_steps=700 \
     wandb.project=act-retint-low \
-    rollout.max_steps=700
+    dryrun=false
